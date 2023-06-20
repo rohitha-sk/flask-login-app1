f9f4c69c5f97cd635a1e2f4357f4cf286bea0999
EC2 Default User <ec2-user@ip-172-31-26-228.ec2.internal>
2023-06-20 13:40:11 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+import key_config as keys
+import boto3 
+import dynamoDB_create_table as dynamodb_ct
+
+app = Flask(__name__)
+
+dynamodb = boto3.resource(
+    'dynamodb',
+     aws_access_key_id = 'AKIA5CFUGMGXXVCEYSXL',
+     aws_secret_access_key = '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',
+     region_name         = 'us-east-1'
+    )
+
+from boto3.dynamodb.conditions import Key, Attr
+
+@app.route('/')
+def index():
+    # dynamodb_ct.create_table()
+    # return 'Table Created'
+    return render_template('index.html')
+    
+@app.route('/login')
+def login():    
+    return render_template('login.html')
+
+@app.route('/signup', methods=['post'])
+def signup():
+    #if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        
+        table = dynamodb.Table('users')
+        
+        table.put_item(
+                Item={
+        'name': name,
+        'email': email,
+        'password': password
+            }
+        )
+        msg = "Registration Complete. Please Login to your account !"
+        return render_template('login.html',msg = msg)
+        
+
+@app.route('/check',methods = ['post'])
+def check():
+    if request.method=='POST':
+        
+        email = request.form['email']
+        password = request.form['password']
+        
+        table = dynamodb.Table('users')
+        response = table.query(
+                KeyConditionExpression=Key('email').eq(email)
+        )
+        items = response['Items']
+        name = items[0]['name']
+        print(items[0]['password'])
+        if password == items[0]['password']:
+            
+            return render_template("home.html",name = name)
+    return render_template("login.html")
+
+
+    
+if __name__ == '__main__':
+    app.run(debug=True,port=8080,host='0.0.0.0')
---
+++ dynamoDB_create_table.py
@@ -0,0 +1,40 @@
+import boto3
+
+import key_config as keys
+
+
+dynamodb_client = boto3.client(
+   'dynamodb',
+    aws_access_key_id = 'AKIA5CFUGMGXXVCEYSXL',
+    aws_secret_access_key = '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',
+    region_name         = 'us-east-1'
+    )
+dynamodb_resource = boto3.resource(
+    'dynamodb',
+    aws_access_key_id = 'AKIA5CFUGMGXXVCEYSXL',
+    aws_secret_access_key = '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',
+    region_name         = 'us-east-1'
+    )
+
+
+def create_table():
+   table = dynamodb_resource.create_table(
+       TableName = 'users', # Name of the table
+       KeySchema = [
+           {
+               'AttributeName': 'email',
+               'KeyType'      : 'HASH' #RANGE = sort key, HASH = partition key
+           }
+       ],
+       AttributeDefinitions = [
+           {
+               'AttributeName': 'email', # Name of the attribute
+               'AttributeType': 'S'   # N = Number (B= Binary, S = String)
+           }
+       ],
+       ProvisionedThroughput={
+           'ReadCapacityUnits'  : 10,
+           'WriteCapacityUnits': 10
+       }
+   )
+   return table
